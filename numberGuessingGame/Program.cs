ee453ca0376773c2d3093cac0d2925f01aa31e44
Andrey Khen <andreykhen@MacBook-Air-Andrey.local>
2026-02-24 01:25:24 +0500

    Initial commit

---
+++ numberGuessingGame/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+
+namespace numberGuessingGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        bool isOpen = true;
+        int min = 1;
+        int max = 101;
+        int guess;
+        int number;
+        int guesses;
+        String response;
+
+        while (isOpen)
+        {
+            guess = 0;
+            guesses = 0;
+            response = "";
+            number = rand.Next(min, max);
+            while (guess != number)
+            {
+                Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Guess: " + guess);
+
+                if (guess > number)
+                {
+                    Console.WriteLine(guess + " your number is too high!");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine(guess + " your number is to low!");
+                }
+                guesses++;
+            }
+
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("You win!");
+            Console.WriteLine("Guesses: " + guesses);
+            
+            Console.WriteLine("Would you play again? (Y/N): ");
+            response = Console.ReadLine();
+            response = response.ToUpper();
+            if (response == "Y")
+            {
+                isOpen = true;
+            }
+            else
+            {
+                isOpen = false;
+            }
+        }
+
+        Console.WriteLine("Thanks for playing!");
+        Console.ReadKey();
+    }
+        
+}
